80023abc6953876e8100b3acc8051d067c42d710
Kaiyu Hsu <im.kaiyu@gmail.com>
2020-11-30 14:51:13 +0800

    use multi command cli

--- packages/proto/README.md
+++ packages/proto/README.md
@@ -26,7 +26,48 @@ USAGE
 <!-- usagestop -->
 # Commands
 <!-- commands -->
+* [`proto @dsstudio/proto generate [interface.json] -t=[react-styled-components]`](#proto-dsstudioproto-generate-interfacejson--treact-styled-components)
+* [`proto help [COMMAND]`](#proto-help-command)
 
+## `proto @dsstudio/proto generate [interface.json] -t=[react-styled-components]`
+
+```
+USAGE
+  $ proto @dsstudio/proto generate [interface.json] -t=[react-styled-components]
+
+ARGUMENTS
+  INTERFACE_PATH  ComponentStyles interface to use. Should be a .json file
+
+OPTIONS
+  -f, --force
+  -h, --help                   show CLI help
+  -o, --outputPath=outputPath  Output folder for your component library
+
+  -t, --template=template      Template to generate your component library with. Should be one of:
+                               [react-styled-components | react-emotion]
+
+  -v, --version                show CLI version
+
+EXAMPLE
+  @dsstudio/proto generate examples/components.json -t=react-styled-components
+```
+
+_See code: [src/commands/generate.ts](https://github.com/tehkaiyu/design-system-studio/blob/v0.0.6/src/commands/generate.ts)_
+
+## `proto help [COMMAND]`
+
+```
+USAGE
+  $ proto help [COMMAND]
+
+ARGUMENTS
+  COMMAND  command to show help for
+
+OPTIONS
+  --all  see all commands in CLI
+```
+
+_See code: [@oclif/plugin-help](https://github.com/oclif/plugin-help/blob/v3.2.0/src/commands/help.ts)_
 <!-- commandsstop -->
 
 # Component Interface
---
+++ packages/proto/src/commands/generate.ts
@@ -0,0 +1,69 @@
+import * as fs from "fs";
+import * as path from "path";
+import { Command, flags } from "@oclif/command";
+import cli from "cli-ux";
+
+export interface ComponentStyles {
+  button: Record<string, string>;
+  text: Record<string, string>;
+}
+
+const templates = ["react-styled-components", "react-emotion"];
+
+export default class Generate extends Command {
+  static description = `scaffold a component library from your ComponentStyles interface`;
+  static usage = `@dsstudio/proto generate [interface.json] -t=[${templates[0]}]`;
+  static examples = [
+    `@dsstudio/proto generate examples/components.json -t=${templates[0]}`,
+  ];
+
+  static flags = {
+    // add --version flag to show CLI version
+    version: flags.version({ char: "v" }),
+    help: flags.help({ char: "h" }),
+    force: flags.boolean({ char: "f" }),
+    // flag with a value (-t, --t=VALUE)
+    template: flags.string({
+      char: "t",
+      description: `Template to generate your component library with. Should be one of: [${templates.join(
+        " | "
+      )}]`,
+    }),
+    outputPath: flags.string({
+      char: "o",
+      description: `Output folder for your component library`,
+    }),
+  };
+
+  static args = [
+    {
+      name: "interface_path",
+      description: "ComponentStyles interface to use. Should be a .json file",
+      required: true,
+    },
+  ];
+
+  async run() {
+    const cwd = process.cwd();
+    const { args, flags } = this.parse(Generate);
+    let { template, outputPath } = flags;
+    if (!template) {
+      template = templates[0];
+      this.log(`No template specified, defaulting to [${templates[0]}]`);
+    }
+    if (!outputPath) {
+      outputPath = `${cwd}/components`;
+      this.log(`No output path specified, defaulting to [${outputPath}]`);
+    }
+
+    cli.action.start("Parsing component interface");
+    const jsonString = fs.readFileSync(path.join(cwd, args.interface_path));
+    const componentStyles: ComponentStyles = JSON.parse(jsonString.toString());
+    cli.action.stop();
+
+    cli.action.start(
+      `Scaffolding components [${Object.keys(componentStyles).join(" | ")}]`
+    );
+    cli.action.stop();
+  }
+}
