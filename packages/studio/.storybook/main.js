10cbec1602fdfbdff7ad66553cc129af1ef86583
Kaiyu Hsu <im.kaiyu@gmail.com>
2020-12-07 05:10:06 +0800

    update api

--- packages/studio/.storybook/main.js
+++ packages/studio/.storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../src/**/*.stories.@(mdx|ts|tsx)"],
   addons: [
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app",
