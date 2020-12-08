bd703d7a310acd3ee843bdc35c97ae541be0bd67
Kaiyu Hsu <im.kaiyu@gmail.com>
2020-12-08 20:56:57 +0800

    Add resizing

--- packages/studio/src/features/editor/EditorPage.tsx
+++ packages/studio/src/features/editor/EditorPage.tsx
@@ -1,9 +1,11 @@
-import styled from "@xstyled/styled-components";
+import { useState } from "react";
 import { Switch, Route, NavLink } from "react-router-dom";
+import { th } from "@xstyled/system";
+import styled from "@xstyled/styled-components";
+import Split from "react-split-grid";
 import {
   Nav,
   NavContainer,
-  PageContainer,
   Flex,
   Button,
   Tabs,
@@ -21,44 +23,63 @@ import { ComponentProperties } from "features/themes/ComponentProperties";
 import { useEditorParams } from "features/editor/editorHooks";
 
 export const EditorPage: React.FC = () => {
+  const [columns, setColumns] = useState("1fr 2px 3fr 2px 1fr");
+
+  const handleDrag = (
+    _direction: "row" | "column",
+    _track: number,
+    gridTemplateStyle: string
+  ) => {
+    setColumns(gridTemplateStyle);
+  };
+
   return (
     <Flex flexDirection="column" height="100vh">
       <EditorPageNav />
-      <Page>
-        <Sidebar side="left">
-          <Switch>
-            <Route path={`/editor/:systemId/tokens/:tokenKey?`}>
-              <TokenNav />
-            </Route>
-            <Route path={`/editor/:systemId/components/:componentKey?`}>
-              <ComponentNav />
-            </Route>
-          </Switch>
-        </Sidebar>
-        <Content>
-          <Switch>
-            <Route exact path={`/editor/:systemId`}>
-              <HomeContent />
-            </Route>
-            <Route path={`/editor/:systemId/tokens/:tokenKey`}>
-              <TokenContent />
-            </Route>
-            <Route path={`/editor/:systemId/components/:componentKey`}>
-              <ComponentContent />
-            </Route>
-          </Switch>
-        </Content>
-        <Sidebar side="right">
-          <Switch>
-            <Route path={`/editor/:systemId/tokens/:tokenKey?`}>
-              <TokenProperties />
-            </Route>
-            <Route path={`/editor/:systemId/components/:componentKey?`}>
-              <ComponentProperties />
-            </Route>
-          </Switch>
-        </Sidebar>
-      </Page>
+      <Split
+        gridTemplateColumns={columns}
+        onDrag={handleDrag}
+        cursor="col-resize"
+        render={({ getGridProps, getGutterProps }: any) => (
+          <Page {...getGridProps()}>
+            <Column>
+              <Switch>
+                <Route path={`/editor/:systemId/tokens/:tokenKey?`}>
+                  <TokenNav />
+                </Route>
+                <Route path={`/editor/:systemId/components/:componentKey?`}>
+                  <ComponentNav />
+                </Route>
+              </Switch>
+            </Column>
+            <Gutter {...getGutterProps("column", 1)} />
+            <Column bg="background">
+              <Switch>
+                <Route exact path={`/editor/:systemId`}>
+                  <HomeContent />
+                </Route>
+                <Route path={`/editor/:systemId/tokens/:tokenKey`}>
+                  <TokenContent />
+                </Route>
+                <Route path={`/editor/:systemId/components/:componentKey`}>
+                  <ComponentContent />
+                </Route>
+              </Switch>
+            </Column>
+            <Gutter {...getGutterProps("column", 3)} />
+            <Column>
+              <Switch>
+                <Route path={`/editor/:systemId/tokens/:tokenKey?`}>
+                  <TokenProperties />
+                </Route>
+                <Route path={`/editor/:systemId/components/:componentKey?`}>
+                  <ComponentProperties />
+                </Route>
+              </Switch>
+            </Column>
+          </Page>
+        )}
+      />
     </Flex>
   );
 };
@@ -93,26 +114,25 @@ const EditorPageNav = () => {
   );
 };
 
-export const Page = styled.section`
+const Page = styled.section`
   display: grid;
   height: 100%;
-  grid-template-areas: "lSidebar content rSidebar";
-  grid-template-columns: 265px 1fr 265px;
 `;
 
-export const Sidebar = styled.section<{ side: "left" | "right" }>`
-  display: flex;
-  flex-direction: column;
-  grid-area: ${({ side }) => (side === "left" ? "lSidebar" : "rSidebar")};
-`;
-
-export const Content = styled(PageContainer)`
-  grid-area: content;
-  background-color: background;
+const Column = styled.sectionBox`
   overflow: auto;
-  border-left-width: normal;
-  border-left-style: solid;
-  border-right-width: normal;
-  border-right-style: solid;
-  border-color: borderColor;
+  width: 100%;
+`;
+
+const Gutter = styled.div`
+  position: relative;
+  background-color: borderColor;
+  cursor: col-resize;
+  transition: ${th("transitions.fast")};
+  transition-property: box-shadow;
+  &:hover {
+    box-shadow: 0 2px 0 0 white, 0 -2px 0 0 white,
+      2px 0 2px 0 ${th("colors.primaryBackground")},
+      -2px 0 2px 0 ${th("colors.primaryBackground")};
+  }
 `;
